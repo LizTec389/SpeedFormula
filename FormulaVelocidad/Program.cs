d89520fa5910f7e51d4d0aff3ba216fe43228864
TecNM <TecNM@DESKTOP-BABKH5A>
2024-02-19 15:35:52 -0600

    Agregar archivos de proyecto.

---
+++ FormulaVelocidad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FormulaFisica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double speed = 100.0; 
+            double time = 5.0; 
+
+            // Speed formula : V = D / T
+             double velocity = speed / time;
+
+            
+            Console.WriteLine($"The speed is {velocity} m/s.");
+        }
+    }
+}
